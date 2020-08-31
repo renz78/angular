8c41b7c37a94354fceb391e06238ba9ab72ea3ff
ae <ae@corp2.net>
2020-09-01 01:21:02 +0300

    users-list1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 
+export interface Section {
+  from: string;
+  subject: string;
+  content: string;
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +12,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'users-list';
+  //messages: string[] = ['Boots', 'Clogs', 'Loafers', 'Moccasins', 'Sneakers'];
+  messages: Section[] = [
+    {
+      from: 'User1',
+      subject: 'subject1',
+      content: 'content1',
+    },
+    {
+      from: 'User2',
+      subject: 'subject2',
+      content: 'content2',
+    },
+    {
+      from: 'User3',
+      subject: 'subject3',
+      content: 'content3',
+    }
+  ];
 }
